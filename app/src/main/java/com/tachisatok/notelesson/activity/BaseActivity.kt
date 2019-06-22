748ead280f7419e10e2b2403a6e81dd7dc6f6191
Takahiro <suzukitakahiro@MacBook-Pro.local>
2019-06-22 23:37:14 +0900

    androidx対応

--- app/src/main/java/com/tachisatok/notelesson/activity/BaseActivity.kt
+++ app/src/main/java/com/tachisatok/notelesson/activity/BaseActivity.kt
@@ -1,7 +1,7 @@
 package com.tachisatok.notelesson.activity
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.tachisatok.notelesson.R
 
 open class BaseActivity : AppCompatActivity() {
