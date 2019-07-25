706133325cbfea5a36c016dac0e99cefed6169bb
Takahiro <suzukitakahiro@MacBook-Pro.local>
2019-07-26 00:21:47 +0900

    ゲームの空Viewを作成

---
+++ app/src/main/java/com/tachisatok/notelesson/view/game/GameActivity.kt
@@ -0,0 +1,6 @@
+package com.tachisatok.notelesson.view.game
+
+import com.tachisatok.notelesson.view.base.BaseActivity
+
+class GameActivity : BaseActivity() {
+}
---
+++ app/src/main/java/com/tachisatok/notelesson/view/game/GamePlayingFragment.kt
@@ -0,0 +1,6 @@
+package com.tachisatok.notelesson.view.game
+
+import androidx.fragment.app.Fragment
+
+class GamePlayingFragment : Fragment() {
+}
