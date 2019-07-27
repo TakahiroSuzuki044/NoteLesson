c984391bd8934188ac9ebbe90f2f6391e6390fd0
Takahiro <suzukitakahiro@MacBook-Pro.local>
2019-07-27 16:31:36 +0900

    コメント修正

--- app/src/main/java/com/tachisatok/notelesson/view/game/GameActivity.kt
+++ app/src/main/java/com/tachisatok/notelesson/view/game/GameActivity.kt
@@ -4,9 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import com.tachisatok.notelesson.R
-import com.tachisatok.notelesson.constant.GameLevel
 import com.tachisatok.notelesson.view.base.BaseActivity
-import com.tachisatok.notelesson.view.select.RangeSelectActivity
 import com.tachisatok.notelesson.view.select.RangeSelectHorizontalItemData
 
 class GameActivity : BaseActivity() {
@@ -21,15 +19,18 @@ class GameActivity : BaseActivity() {
 
     companion object {
         /**
-         * INTENT KEY：選択した[GameLevel]
+         * INTENT KEY：選択した[RangeSelectHorizontalItemData]
          */
         private const val INTENT_KEY_ITEM_DATA = "intent_key_range_select_horizontal_item_data"
 
         /**
-         * [RangeSelectActivity]を起動するIntentを返却する
+         * [GameActivity]を起動するIntentを返却する
          */
         @JvmStatic
-        fun newIntent(context: Context, rangeSelectHorizontalItemData: RangeSelectHorizontalItemData): Intent {
+        fun newIntent(
+            context: Context,
+            rangeSelectHorizontalItemData: RangeSelectHorizontalItemData
+        ): Intent {
             return Intent(context, GameActivity::class.java)
                 .putExtra(INTENT_KEY_ITEM_DATA, rangeSelectHorizontalItemData)
         }
