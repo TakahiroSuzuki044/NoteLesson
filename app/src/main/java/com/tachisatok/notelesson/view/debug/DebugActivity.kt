e5005da49c8cce3510965467462ae99af197bf30
Takahiro <suzukitakahiro@MacBook-Pro.local>
2019-09-08 18:28:26 +0900

    デバッグ画面の追加

--- app/src/main/java/com/tachisatok/notelesson/view/debug/DebugActivity.kt
+++ app/src/main/java/com/tachisatok/notelesson/view/debug/DebugActivity.kt
@@ -1,7 +1,45 @@
 package com.tachisatok.notelesson.view.debug
 
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.crashlytics.android.Crashlytics
+import com.tachisatok.notelesson.R
 import com.tachisatok.notelesson.view.base.BaseActivity
+import kotlinx.android.synthetic.main.debug_activity.*
 
-class DebugActivity: BaseActivity() {
+class DebugActivity : BaseActivity(), View.OnClickListener {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.debug_activity)
+
+        initView()
+    }
+
+    /**
+     * Viewの初期化処理
+     */
+    private fun initView() {
+        setView(force_crash_layout, R.string.force_crash)
+    }
+
+    /**
+     * Viewの各設定を行う
+     */
+    private fun setView(v: View, res: Int) {
+        v.isClickable = true
+        v.isFocusable = true
+        v.setOnClickListener(this)
+        val textView = v.findViewById<TextView>(R.id.section_list_text_view)
+        textView.text = getString(res)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.force_crash_layout -> {
+                Crashlytics.getInstance().crash()
+            }
+        }
+    }
 }
